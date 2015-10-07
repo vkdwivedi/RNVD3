5675a635832d5728cfcf9f09c90b484de54e3a05
Vineet Dwivedi <jun@amogha.com>
2015-10-07 18:09:29 +0530

    Update renderChart.R

--- R/renderChart.R
+++ R/renderChart.R
@@ -1,6 +1,6 @@
 #' renderChart (use with Shiny)
 #' 
-#' Use rNVD3 charts as Shiny output. First, use \code{renderChart} in
+#' Use RNVD3 charts as Shiny output. First, use \code{renderChart} in
 #' \code{server.R} to assign the chart object to an Shiny output. Then create an
 #' chartOutput with the same name in \code{ui.R}. \code{chartOutput} is
 #' currently just an alias for \code{htmlOutput}. 
