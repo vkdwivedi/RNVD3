9defdb2b6d11b623343b601354471f95fd3062ce
Vineet Dwivedi <jun@amogha.com>
2015-10-07 16:37:43 +0530

    Update showOutput.R

--- R/showOutput.R
+++ R/showOutput.R
@@ -5,11 +5,11 @@ showOutput <- function(outputId) {
   div(class="NVD3", 
     # Add Javascripts
     tagList(
-     singleton(tags$head(tags$script(src = "nvd3/js/d3.v2.min.js", type='text/javascript'))),
-     singleton(tags$head(tags$script(src = "nvd3/js/nv.d3.js", type='text/javascript'))),
+     singleton(tags$head(tags$script(src = "nvd3/js/d3.min.js", type='text/javascript'))),
+     singleton(tags$head(tags$script(src = "nvd3/js/nv.d3.min.js", type='text/javascript'))),
      singleton(tags$head(tags$script(src = "nvd3/js/fisheye.js", type='text/javascript'))),
-     singleton(tags$head(tags$link(href = "nvd3/css/nv.d3.css", rel="stylesheet"))),
-     singleton(tags$head(tags$link(href = "nvd3/css/rNVD3.css", rel="stylesheet")))
+     singleton(tags$head(tags$link(href = "nvd3/css/nv.d3.min.css", rel="stylesheet"))),
+     singleton(tags$head(tags$link(href = "nvd3/css/RNVD3.css", rel="stylesheet")))
     ),
     # Add chart html
     htmlOutput(outputId)
